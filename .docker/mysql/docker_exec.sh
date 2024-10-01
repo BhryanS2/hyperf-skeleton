a3cfa55f62bf20c5c9fdfc062dae3aad18282d34
bhryan.cruz <bhryan.cruz@mova.vc>
2024-10-01 12:03:00 -0300

    fix: update database  with ssl

--- .docker/mysql/docker_exec.sh
+++
@@ -1,2 +0,0 @@
-ID=$(docker ps | grep "docker_mysql_ssl" | awk '{ print $1 }')
-docker exec -it $ID bash
--- .docker/mysql/docker_remove.sh
+++
@@ -1,3 +0,0 @@
-docker service rm docker_mysql_ssl
-docker secret rm db_root_password
-sudo rm -rf mysql
